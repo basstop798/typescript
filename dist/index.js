a6bb3e9eb75b468f411463563c490b540732c74f
Abdelbasset Lettreuch <abdelbassetlettreuch5@gmail.com>
2025-12-30 13:05:50 +0100

    typescript commit message

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    start() {
+        console.log("Car engine started");
+    }
+}
+const myCar = new Car("Toyota", "Corolla", 2020);
+myCar.start();
